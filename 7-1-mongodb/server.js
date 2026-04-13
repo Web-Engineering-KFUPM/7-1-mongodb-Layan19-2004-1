0954bb771e335495153b700d159cf7cd995fc8a0
Layan-2004 <layanalasmari25@gmail.com>
2026-04-13 13:45:31 +0300

    todo4(2)

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -210,7 +210,11 @@ async function createStudents() {
       createStudents();
 
 // read document
-
+async function readStudents() {
+         const all = await Student.find();
+         console.log(all);
+      }
+      readStudents();
 
 // update document
 
